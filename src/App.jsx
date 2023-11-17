d5fb4277d087039cc6b16a95f228a4b6173aa8d3
Jess <jessjelardo@gmail.com>
2023-11-17 20:14:21 +0800

    server.js - mock api by mirage.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 
+import "./server"
+
 function App() {
   return (
     <BrowserRouter>
