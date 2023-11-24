f79401a19e8d05870801d27ed1ffb81dbf7380ee
Jess <jessjelardo@gmail.com>
2023-11-24 19:23:38 +0800

    cars+conditional renders addition

--- src/pages/Car/Cars.jsx
+++ src/pages/Car/Cars.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import React from "react"
 
 /**
@@ -9,6 +9,9 @@ import React from "react"
 
 export default function Cars(){
 
+    const [searchParams, setSearchParams] = useSearchParams()
+    const typeFilter = searchParams.get("type")
+
     const [cars, setCars] = React.useState([])
 
     React.useEffect(() => {
@@ -18,7 +21,7 @@ export default function Cars(){
     }, [])
 
 
-    const carElements = cars.map(car => (
+    const carElements = cars.filter(car => typeFilter ? car.type === typeFilter : true).map(car => (
         <div key={car.id} className="car-tile">
             <Link to={`/cars/${car.id}`}>
                 <img src={car.imageUrl} />
@@ -31,9 +34,48 @@ export default function Cars(){
         </div>
     ))
 
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
+
+
+
     return (
         <div className="car-list-container">
             <h1>Explore our car options</h1>
+            <div className="car-list-filter-buttons">
+                <button
+                    onClick={() => handleFilterChange("type", "simple")}
+                    className={`car-type simple ${typeFilter === "simple" ? "selected" : null}`}
+                >
+                    Simple
+                </button>
+                <button
+                    onClick={() => handleFilterChange("type", "luxury")}
+                    className={`car-type luxury ${typeFilter === "luxury" ? "selected" : null}`} 
+                >
+                    Luxury
+                </button>
+                <button
+                    onClick={() => handleFilterChange("type", "rugged")}
+                    className={`car-type rugged ${typeFilter === "rugged" ? "selected" : null}`} 
+                >
+                    Rugged
+                </button>
+                {typeFilter && <button
+                    onClick={() => handleFilterChange("type", null)}
+                    className="car-type clear-filters" 
+                >
+                    Clear filter
+                </button>}
+            </div>
             <div className="car-list">
                 {carElements}
             </div>
